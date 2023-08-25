0618ab08d227cd874a3378a174434fe69de757dd
Yasar Chavez <tneniitoo@gmail.com>
2023-08-24 21:01:51 -0300

    Update

--- src/components/pure/greetingF.jsx
+++ src/components/pure/greetingF.jsx
@@ -3,13 +3,16 @@ import PropTypes from "prop-types";
 
 const GreetingF = (props) => {
   // Use state
-  // const [variable, metodo para actualizarla] = useState(valor inicial);
+  // const [variable a modificar, metodo para actualizarla] = useState(valor inicial);
   const [age, setage] = useState(29);
 
   const birthday = () => {
     //Actualizar el state
     setage(age + 1);
   };
+  const quitarAnios=()=>{
+    setage(age-1);
+  }
 
   return (
     <div>
@@ -17,6 +20,9 @@ const GreetingF = (props) => {
       <h2>Tu edad es: {age}</h2>
       {/* Llamamos al metodo */}
       <button onClick={birthday}>Cumplir Años</button>
+      <button onClick={quitarAnios}>Quitar Años</button>
+      {/* <button onMouseOver={quitarAnios}>Quitar Años</button> */}
+      
     </div>
   );
 };
