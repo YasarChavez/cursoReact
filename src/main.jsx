63f08171b8c5f6abb6e102c7f3993dc014d52d0c
Yasar Chavez <tneniitoo@gmail.com>
2023-08-27 11:11:43 -0300

    Upd

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+//Añadimos Bootstrap a nuestro proyecto //npm install bootstrap o npm install bootstrap --save
+import 'bootstrap/dist/css/bootstrap.css'
+//Importante: Los estilos propios deben ir debajo de bootstrap
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
